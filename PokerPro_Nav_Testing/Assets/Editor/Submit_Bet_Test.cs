b7df82414077fee02dee5d5113878cc238e75175
k <jdsiebert@wisc.edu>
2017-04-06 22:31:30 -0500

    submit bet test changes
    -all 4 unit tests for submit bet pass

--- PokerPro_Nav_Testing/Assets/Editor/Submit_Bet_Test.cs
+++ PokerPro_Nav_Testing/Assets/Editor/Submit_Bet_Test.cs
@@ -6,6 +6,83 @@ using UnityEngine.UI;
 public class Submit_Bet_Test {
 
     [Test]
+    public void allInTest()
+    {
+        //Arrange
+        // DOCs
+        FakeServer server = new FakeServer();
+        FakeSlider slider = new FakeSlider();
+        FakeDisplayMessage messages = new FakeDisplayMessage();
+        FakeDisableButtons buttons = new FakeDisableButtons();
+        // get a new set of global vars
+        FakeGlobalVars globals = new FakeGlobalVars();
+        globals.player_id = "1";
+        globals.game_id = "2";
+        globals.chips = 2000;
+        globals.bet = globals.chips;
+
+        // set up SUT
+        var test_fixture = new GameObject();
+        test_fixture.AddComponent<SubmitBet_Tester>();
+        SubmitBet_Tester.setUp(server, slider, globals, messages, buttons, false);
+
+        //Act
+        //Try to sent a bet
+        SubmitBet_Tester.allIn();
+
+        //Assert
+        // correct message is displayed
+        Assert.AreEqual("You have bet 2000.", messages.messages[0]);
+        // slider is reset and deactivated
+        Assert.AreEqual(0, slider.value);
+        Assert.False(slider.interactable);
+        // correct url sent to the server
+        Assert.AreEqual("http://104.131.99.193/game/2/1/2000", server.url);
+        // buttons are deactivated
+        Assert.False(buttons.interactable);
+        // current bet reset
+        Assert.AreEqual(0, globals.curr_bet);
+    }
+
+    [Test]
+    public void callTest()
+    {
+        //Arrange
+        // DOCs
+        FakeServer server = new FakeServer();
+        FakeSlider slider = new FakeSlider();
+        FakeDisplayMessage messages = new FakeDisplayMessage();
+        FakeDisableButtons buttons = new FakeDisableButtons();
+        // get a new set of global vars
+        FakeGlobalVars globals = new FakeGlobalVars();
+        globals.player_id = "1";
+        globals.game_id = "2";
+        globals.curr_bet = 500;
+        globals.bet = globals.curr_bet;
+
+        // set up SUT
+        var test_fixture = new GameObject();
+        test_fixture.AddComponent<SubmitBet_Tester>();
+        SubmitBet_Tester.setUp(server, slider, globals, messages, buttons, false);
+
+        //Act
+        //Try to send a call
+        SubmitBet_Tester.call();
+
+        //Assert
+        // correct message is displayed
+        Assert.AreEqual("You have bet 500.", messages.messages[0]);
+        // slider is reset and deactivated
+        Assert.AreEqual(0, slider.value);
+        Assert.False(slider.interactable);
+        // correct url sent to the server
+        Assert.AreEqual("http://104.131.99.193/game/2/1/500", server.url);
+        // buttons are deactivated
+        Assert.False(buttons.interactable);
+        // current bet reset
+        Assert.AreEqual(0, globals.curr_bet);
+    }
+    [Test]
     public void foldTest()
     {
         //Arrange
@@ -16,13 +93,12 @@ public class Submit_Bet_Test {
         FakeDisableButtons buttons = new FakeDisableButtons();
         // get a new set of global vars
         FakeGlobalVars globals = new FakeGlobalVars();
-        GlobalVars.player_id = "1";
-        GlobalVars.game_id = "2";
+        globals.player_id = "1";
+        globals.game_id = "2";
 
         // set up SUT
         var test_fixture = new GameObject();
         test_fixture.AddComponent<SubmitBet_Tester>();
-        test_fixture.GetComponent<SubmitBet_Tester>().Awake();
         SubmitBet_Tester.setUp(server, slider, globals, messages, buttons, true);
 
         //Act
@@ -30,7 +106,17 @@ public class Submit_Bet_Test {
         SubmitBet_Tester.fold();
 
         //Assert
-        Assert.IsNull(server.text);
+        // correct message is displayed
+        Assert.AreEqual("You have folded.", messages.messages[0]);
+        // slider is reset and deactivated
+        Assert.AreEqual(0, slider.value);
+        Assert.False(slider.interactable);
+        // correct url sent to the server
+        Assert.AreEqual("http://104.131.99.193/game/2/1/0", server.url);
+        // buttons are deactivated
+        Assert.False(buttons.interactable);
+        // current bet reset
+        Assert.AreEqual(0, globals.curr_bet);
     }
 
     [Test]
@@ -50,7 +136,6 @@ public class Submit_Bet_Test {
         // set up SUT
         var test_fixture = new GameObject();
         test_fixture.AddComponent<SubmitBet_Tester>();
-        test_fixture.GetComponent<SubmitBet_Tester>().Awake();
         SubmitBet_Tester.setUp(server, slider, globals, messages, buttons, false);
 
         //Act
@@ -61,10 +146,15 @@ public class Submit_Bet_Test {
         // correct message is displayed
         Assert.AreEqual("You have bet 1000.", messages.messages[0]);
         // slider is reset and deactivated
-        Assert.AreEqual(slider.value, 0);
+        Assert.AreEqual(0, slider.value);
         Assert.False(slider.interactable);
         // correct url sent to the server
         Assert.AreEqual("http://104.131.99.193/game/2/1/1000", server.url);
+        // buttons are deactivated
+        Assert.False(buttons.interactable);
+        // current bet reset
+        Assert.AreEqual(0, globals.curr_bet);
+
         
 	}
 }
